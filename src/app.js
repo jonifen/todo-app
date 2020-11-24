1233415b26b22fca73659289027b38982f5f8133
Jon Cain <jonifen@users.noreply.github.com>
2020-11-24 20:18:39 +0000

    Wire up the reducer and context to make available within the app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { render } from "react-dom";
+import TasksContext from "./contexts/tasks-context";
+import { defaultState } from "./defaultState";
+import { tasksReducer } from "./reducers/tasks-reducer";
+import TaskList from "./components/task-list.jsx";
 
 const App = () => {
+  const [tasks, dispatch] = useReducer(tasksReducer, defaultState);
   return (
-    <div>Todo App</div>
+    <React.StrictMode>
+      <TasksContext.Provider value={[tasks, dispatch]}>
+        <div>
+          <header>
+            <h1>Todo App</h1>
+          </header>
+          <TaskList />
+        </div>
+      </TasksContext.Provider>
+    </React.StrictMode>
   );
 };
 
