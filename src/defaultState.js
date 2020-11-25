bb1151d3f8833d73b5646bf44c0c0bd108df38d5
Jon Cain <jonifen@users.noreply.github.com>
2020-11-25 00:28:28 +0000

    Reset the defaultState to something more random

--- src/defaultState.js
+++ src/defaultState.js
@@ -1,26 +1,14 @@
 export const defaultState = {
-  "a1": {
-    task: "add a new task",
+  "77378800-0b1f-4efe-8320-ad7e6ec8e3d4": {
+    task: "Mow the lawn",
+    completed: false
+  },
+  "f286dfa4-f59b-4395-8b03-390faaee7b57": {
+    task: "Make a todo app",
     completed: true
   },
-  "b": {
-    task: "delete a task",
-    completed: true
-  },
-  "c": {
-    task: "edit a task",
-    completed: true
-  },
-  "d": {
-    task: "mark a task as complete",
-    completed: true
-  },
-  "e": {
-    task: "mark a completed task as incomplete",
-    completed: true
-  },
-  "f": {
-    task: "view all tasks (toggle completed?)",
-    completed: true
+  "ca9d1a89-9625-4322-843f-e41e03881387": {
+    task: "Decorate the hallway",
+    completed: false
   }
 };
